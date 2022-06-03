350b05be47cada86ed0ffdb3f25f8251cfbda31e
pakavi <pakavi@protonmail.ch>
2022-06-03 12:23:10 +0300

    Added error page w/ styles

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Wrapper from "../assets/wrappers/ErrorPage";
+import notFound from "../assets/images/not-found.svg";
 
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={notFound} alt="Not found" />
+        <h3>Page Not Found</h3>
+        <Link to="/">Go back</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
