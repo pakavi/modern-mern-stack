ee922ebf1c71220d6da03d12a02a1514dce7b4b2
pakavi <pakavi@protonmail.ch>
2022-06-07 14:32:13 +0300

    Connect client and server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -17,10 +18,11 @@ dotenv.config();
 
 
 const app = express();
-
 app.use(express.json());
+
 app.use(morgan("dev"));
 app.use(helmet());
+app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
